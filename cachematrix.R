a27da5ca0a910b707d7357aeac05d04f8ecc7a5a
NikolaMedakovic <nikola.medakovic@gmail.com>
2014-07-24 08:29:44 +0200

    Added breaklines in comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,6 @@
-## The first function enables the second function to get and set the matrix and its inverse. The second function first tries to access the cached inverse matrix. If it's cached then it is returned and otherwise it is first calculated and then returned.
+## The first function enables the second function to get and set the matrix and its inverse. 
+## The second function first tries to access the cached inverse matrix. 
+## If it's cached then it is returned and otherwise it is first calculated and then returned.
 
 
 ## This function returns a list of functions that gets or sets the matrix as well as getting and setting its inverse.
@@ -18,7 +20,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## This function tries to get the inverse matrix of x. If it is cached it simply returns it. If it is not cached the function first calculates the inverse, caches the solution and returns it.
+## This function tries to get the inverse matrix of x. If it is cached it simply returns it. 
+## If it is not cached the function first calculates the inverse, caches the solution and returns it.
 cacheSolve <- function(x, ...) { 
     i <- x$getinverse()
     if(!is.null(i)) {
